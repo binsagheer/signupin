c2b03337480fefcb1b3b7ac9a3f4fe6140594c8f
binsagheer <dev.umair1018@gmail.com>
2024-08-08 10:51:22 +0500

    my life my rule

---
+++ scripit.js
@@ -0,0 +1,10 @@
+//  for hinde and show password
+$(".toggle-password").click(function() {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    input = $(this).parent().find("input");
+    if (input.attr("type") == "password") {
+        input.attr("type", "text");
+    } else {
+        input.attr("type", "password");
+    }
+});
